9bc38010553651bf23a55e9877c5f50415e872ec
edsonjaramillo <edsonajaramillo@gmail.com>
2022-05-10 08:20:02 -0500

    added logging for exception checking

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from scrapers.teams_scraper import TeamsScraper
 from scrapers.stats_scraper import StatsScraper
 from scrapers.games_today_scraper import GamesTodayScraper
 from components.cms import CMS
+from components.logger import Logger
 
 
 def main() -> None:
@@ -10,13 +11,17 @@ def main() -> None:
 
     browser = Browser()
     browser.start_browser(is_headless=True)
-    has_teams, teams_playing = GamesTodayScraper(browser).get_games()
-    has_teams = True
-    if has_teams == True:
-        batters = TeamsScraper(browser).get_batters(NUM_BATTERS, teams_playing)
-        final_batters = StatsScraper(browser).get_stats(batters)
-        CMS().update_cms(final_batters)
-    browser.close_browser()
+    try:
+        has_teams, teams_playing = GamesTodayScraper(browser).get_games()
+        has_teams = True
+        if has_teams == True:
+            batters = TeamsScraper(browser).get_batters(NUM_BATTERS, teams_playing)
+            final_batters = StatsScraper(browser).get_stats(batters)
+            CMS().update_cms(final_batters)
+    except Exception as e:
+        Logger().report_exception(e)
+    finally:
+        browser.close_browser()
 
 
 if __name__ == "__main__":
